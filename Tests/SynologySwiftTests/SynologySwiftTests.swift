f3b7e260a685cc1df527b23030e7e5db9bdf91ac
Thomas Le Gravier <thomas.le-gravier@happn.fr>
2019-05-19 09:51:10 +0200

    Add unit tests

--- Tests/SynologySwiftTests/SynologySwiftTests.swift
+++ Tests/SynologySwiftTests/SynologySwiftTests.swift
@@ -34,15 +34,42 @@ class SynologySwiftTests: XCTestCase {
         
         let delayExpectation = expectation(description: "Waiting SynologySwiftAuth")
         
-        SynologySwift.login(quickConnectid: "your-quickConnect-id", login: "login", password: "password") { (result) in
+        SynologySwift.login(quickConnectid: "your-quick-id", sessionType: "DownloadStation", login: "dsdownload", password: "your-password", useDefaultCacheApis: true) { (result) in
             switch result {
             case .success(_): XCTAssertTrue(true)
-            case .failure(_):   XCTAssertTrue(false)
+            case .failure(let error): print(error); XCTAssertTrue(false)
             }
             delayExpectation.fulfill()
         }
 
         waitForExpectations(timeout: 30.0, handler: nil)
     }
-
+    
+    func testPing() {
+        let delayExpectation = expectation(description: "Waiting ping")
+        
+        let dsInfos = SynologySwiftURLResolver.DSInfos(quickId: "your-quick-id", host: "your-quick-id.synology.me", port: 5000)
+        SynologySwift.ping(dsInfos: dsInfos) { (result) in
+            switch result {
+            case .success(_): XCTAssertTrue(true)
+            case .failure(let error): print(error); XCTAssertTrue(false)
+            }
+            delayExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+    
+    func testlogout() {
+        let delayExpectation = expectation(description: "Waiting logout")
+        
+        let dsAuthInfos = SynologySwiftAuth.DSAuthInfos(sid: "your-sid", account: "your-account", dsInfos: SynologySwiftURLResolver.DSInfos(quickId: "your-quick-id", host: "host", port: 5000))
+        SynologySwift.logout(dsAuthInfos: dsAuthInfos, sessionType: "DownloadStation") { (result) in
+            switch result {
+            case .success(_): XCTAssertTrue(true)
+            case .failure(let error): print(error); XCTAssertTrue(false)
+            }
+            delayExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 15.0, handler: nil)
+    }
 }
